381d1bc38c40842320d7abd73e2407aa0bbd01fa
vsync <vsync.design@gmail.com>
2018-03-18 20:21:38 +0200

    fixes #53

--- dist/tagify.js
+++ dist/tagify.js
@@ -1,5 +1,5 @@
 /**
- * Tagify (v 1.2.2)- tags input component
+ * Tagify (v 1.2.4)- tags input component
  * By Yair Even-Or (2016)
  * Don't sell this code. (c)
  * https://github.com/yairEO/tagify
