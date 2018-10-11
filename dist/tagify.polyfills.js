0472ecc39a47531eb942905b3bd84b86c59c19ad
yaireo <vsync.design@gmail.com>
2018-10-11 08:39:35 +0300

    fixes #120

--- dist/tagify.polyfills.js
+++ dist/tagify.polyfills.js
@@ -1,67 +1,70 @@
 (function(){
-        if( !String.prototype.includes ){
-                String.prototype.includes = function(search, start) {
-                        'use strict';
-                        if (typeof start !== 'number')
-                                start = 0;
-
-                        if (start + search.length > this.length)
-                                return false;
-
-                        else
-                                return this.indexOf(search, start) !== -1;
-                };
-        }
-
-        //////////////////////////////////////////////////////////////////
-        // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
-        if ( typeof window.CustomEvent === "function" ) return false;
-
-        function CustomEvent ( event, params ) {
-                params = params || { bubbles: false, cancelable: false, detail: undefined };
-                var evt = document.createEvent( 'CustomEvent' );
-                evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-                return evt;
-        }
-
-        CustomEvent.prototype = window.Event.prototype;
-
-        window.CustomEvent = CustomEvent;
-
-
-        //////////////////////////////////////////////////////////////////
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-        //
-        if (typeof Object.assign != 'function') {
-            // Must be writable: true, enumerable: false, configurable: true
-            Object.defineProperty(Object, "assign", {
-                value: function assign(target, varArgs) { // .length of function is 2
+    if( !String.prototype.includes ){
+            String.prototype.includes = function(search, start) {
                     'use strict';
-                    if (target == null) { // TypeError if undefined or null
-                        throw new TypeError('Cannot convert undefined or null to object');
-                    }
+                    if (typeof start !== 'number')
+                            start = 0;
 
-                    var to = Object(target);
+                    if (start + search.length > this.length)
+                            return false;
 
-                    for (var index = 1; index < arguments.length; index++) {
-                        var nextSource = arguments[index];
+                    else
+                            return this.indexOf(search, start) !== -1;
+            };
+    }
 
-                        if (nextSource != null) { // Skip over if undefined or null
-                            for (var nextKey in nextSource) {
-                                // Avoid bugs when hasOwnProperty is shadowed
-                                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                                    to[nextKey] = nextSource[nextKey];
-                                }
+    //////////////////////////////////////////////////////////////////
+    // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+    if ( typeof window.CustomEvent === "function" ) return false;
+
+    function CustomEvent ( event, params ) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            var evt = document.createEvent( 'CustomEvent' );
+            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+            return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEvent;
+
+
+    //////////////////////////////////////////////////////////////////
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+    //
+    if (typeof Object.assign != 'function') {
+        // Must be writable: true, enumerable: false, configurable: true
+        Object.defineProperty(Object, "assign", {
+            value: function assign(target, varArgs) { // .length of function is 2
+                'use strict';
+                if (target == null) { // TypeError if undefined or null
+                    throw new TypeError('Cannot convert undefined or null to object');
+                }
+
+                var to = Object(target);
+
+                for (var index = 1; index < arguments.length; index++) {
+                    var nextSource = arguments[index];
+
+                    if (nextSource != null) { // Skip over if undefined or null
+                        for (var nextKey in nextSource) {
+                            // Avoid bugs when hasOwnProperty is shadowed
+                            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                                to[nextKey] = nextSource[nextKey];
                             }
                         }
                     }
-                    return to;
-                },
-                writable: true,
-                configurable: true
-            });
-        }
+                }
+                return to;
+            },
+            writable: true,
+            configurable: true
+        });
+    }
 
+    if( window.NodeList && !NodeList.prototype.forEach ){
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
 
     if (!Array.prototype.findIndex) {
         Object.defineProperty(Array.prototype, 'findIndex', {
