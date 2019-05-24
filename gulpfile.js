d51fb0c8388f7af8367d91aa55471f3240242f3f
Yair Even Or <vsync.design@gmail.com>
2019-05-25 00:20:35 +0300

    added gulp tasks for automatic versioning

--- gulpfile.js
+++ gulpfile.js
@@ -192,6 +192,37 @@ gulp.task('lint_js', () => {
 });
 
 
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src('./package.json')
+        // bump the version number in those files
+        .pipe($.bump({type: importance}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        .pipe($.git.commit('bumps package version'))
+
+        // read only one file to get the version number
+        .pipe($.filter('package.json'))
+        // **tag it in the repository**
+        .pipe($.tagVersion());
+}
+
+
 gulp.task('watch', () => {
     //gulp.watch('./images/sprite/**/*.png', ['sprite']);
     gulp.watch('./src/*.scss', ['scss']);
@@ -202,3 +233,8 @@ gulp.task('watch', () => {
 gulp.task('default', ( done ) => {
     $.runSequence(['build_js', 'scss'], 'build_jquery_version', 'minify', 'watch', done);
 });
+
+
+gulp.task('patch', () => inc('patch'))
+gulp.task('feature', () => inc('minor'))
+gulp.task('release', () => inc('major'))
