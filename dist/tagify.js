cf79f76fd1b266d56eb35126241eb665107f3c1a
vsync <vsync.design@gmail.com>
2020-02-25 19:40:28 +0200

    bumps package version

--- dist/tagify.js
+++ dist/tagify.js
@@ -1711,7 +1711,7 @@ Tagify.prototype = {
     this.preUpdate();
     var value = removeCollectionProp(this.value, "__isValid");
     this.DOM.originalInput.value = this.settings.mode == 'mix' ? this.getMixedTagsAsString(value) : this.value.length ? JSON.stringify(value) : "";
-    this.DOM.originalInput.dispatchEvent(new Event('change'));
+    this.DOM.originalInput.dispatchEvent(new CustomEvent('change'));
   },
   getMixedTagsAsString: function getMixedTagsAsString(value) {
     var result = "",
--- src/tagify.polyfills.js
+++ src/tagify.polyfills.js
@@ -14,19 +14,18 @@
 
     //////////////////////////////////////////////////////////////////
     // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
-    if ( typeof window.CustomEvent === "function" ) return false;
+    if ( typeof window.CustomEvent !== "function" ){
+      function CustomEvent ( event, params ) {
+              params = params || { bubbles: false, cancelable: false, detail: undefined };
+              var evt = document.createEvent( 'CustomEvent' );
+              evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+              return evt;
+      }
 
-    function CustomEvent ( event, params ) {
-            params = params || { bubbles: false, cancelable: false, detail: undefined };
-            var evt = document.createEvent( 'CustomEvent' );
-            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-            return evt;
+      CustomEvent.prototype = window.Event.prototype;
+
+      window.CustomEvent = CustomEvent;
     }
-
-    CustomEvent.prototype = window.Event.prototype;
-
-    window.CustomEvent = CustomEvent;
-
 
     //////////////////////////////////////////////////////////////////
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
