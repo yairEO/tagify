2372dee45f0c8aba8b1ef7031db8ebe7f0ce1090
vsync <vsync.design@gmail.com>
2020-08-08 23:58:39 +0300

    checkboxes for readmap

--- docs/codepen.js
+++ docs/codepen.js
@@ -1,11 +1,12 @@
 (function(){
+    var repoLinkContent = `<img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png' alt='GitHub Repo'>`;
+    repoLinkContent = `https://github.com/yairEO/tagify/raw/master/docs/readme-header.svg`
     var repoLink = `<a class='repoLink' title='Go To Repo' href='https://github.com/yairEO/tagify'>
-                        <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png' alt='GitHub Repo'>
+                    ${repoLinkContent}
                     </a>`
 
     var intro = `<section class="pen-intro">
         ${repoLink}
-        <h1>Tagify</h1>
     </section>`
 
     document.body.insertAdjacentHTML('beforeend', intro)
