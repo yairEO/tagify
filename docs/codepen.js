afa380810184d06376a9e7bae513382f787c2701
vsync <vsync.design@gmail.com>
2020-08-09 00:28:56 +0300

    -

--- docs/codepen.js
+++ docs/codepen.js
@@ -1,14 +1,17 @@
 (function(){
     var repoLinkContent = `<img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png' alt='GitHub Repo'>`
     repoLinkContent = `<img src="https://github.com/yairEO/tagify/raw/master/docs/readme-header.svg" width="100%" height="160">`
-    
+
     var repoLink = `<a class='repoLink' title='Go To Repo' href='https://github.com/yairEO/tagify'>
                         ${repoLinkContent}
                     </a>`
 
-    var intro = `<section class="pen-intro">
-        ${repoLink}
-    </section>`
+    var html = `
+        <label for='checkbox-tagify-show-input'>Show original input</label>
+        <input type='checkbox' id='checkbox-tagify-show-input'>
+        <section class="pen-intro">
+            ${repoLink}
+        </section>`
 
-    document.body.insertAdjacentHTML('beforeend', intro)
+    document.body.insertAdjacentHTML('afterbegin', html)
 })()
