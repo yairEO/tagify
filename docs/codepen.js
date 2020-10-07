dcc467f4ce55a77bd29c5a6ef97a91a67c3a3657
Yair Even Or <yair.evenor@5rr5.co>
2020-10-07 19:51:17 +0200

    minors

--- docs/codepen.js
+++ docs/codepen.js
@@ -1,6 +1,6 @@
 (function(){
     var repoLinkContent = `<img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png' alt='GitHub Repo'>`
-    repoLinkContent = `<img src="https://github.com/yairEO/tagify/raw/master/docs/readme-header.svg" width="100%" height="160">`
+    repoLinkContent = `<img src="https://github.com/yairEO/tagify/raw/master/docs/readme-header.svg" width="100%" height="100%">`
 
     var repoLink = `<a class='repoLink' title='Go To Repo' href='https://github.com/yairEO/tagify'>
                         ${repoLinkContent}
