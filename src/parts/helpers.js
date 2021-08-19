f5c1f71bec0afbf01dcb3d54a95c28f5b1b74762
Gatis Lapsiņš <gatis.lapsins@visma.com>
2021-08-19 13:43:58 +0300

    #888 IE suppot
    Implement tagify IE11+ support:
    - Rewrite browserlist to include IE11
    - Rewrite crypto useage. On Microsoft product it use msCrypto

--- src/parts/helpers.js
+++ src/parts/helpers.js
@@ -173,6 +173,6 @@ export var isChromeAndroidBrowser = () => /(?=.*chrome)(?=.*android)/i.test(navi
 
 export function getUID() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        (c ^ ( window.crypto || window.msCrypto ).getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     )
 }
