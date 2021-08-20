7564204eebaf772515e15742e059eec80edf771c
lagrawala <lalit.agrawala@vbrick.com>
2021-08-20 17:58:40 -0400

    IE11 - Added window.msCrypto condition as window.crypto not available in IE11.

--- src/parts/helpers.js
+++ src/parts/helpers.js
@@ -172,7 +172,9 @@ export function getNodeHeight( node ){
 export var isChromeAndroidBrowser = () => /(?=.*chrome)(?=.*android)/i.test(navigator.userAgent)
 
 export function getUID() {
+    //handling crypto for IE11.
+    var cryptoObj = window.crypto || window.msCrypto;
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        (c ^ cryptoObj.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     )
 }
