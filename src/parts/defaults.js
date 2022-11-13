c314aebaa4d8762b4b8af763284017dec8417f3a
Yair Even Or <Yair.Even-Or@amdocs.com>
2022-11-13 23:08:38 +0200

    chore - typo

--- src/parts/defaults.js
+++ src/parts/defaults.js
@@ -14,7 +14,7 @@ export default {
     keepInvalidTags     : false,          // if true, do not remove tags which did not pass validation
     createInvalidTags   : true,           // if false, do not create invalid tags from invalid user input
     mixTagsAllowedAfter : /,|\.|\:|\s/,   // RegEx - Define conditions in which mix-tags content allows a tag to be added after
-    mixTagsInterpolator : ['[[', ']]'],   // Interpolation for mix mode. Everything between this will becmoe a tag
+    mixTagsInterpolator : ['[[', ']]'],   // Interpolation for mix mode. Everything between these will become a tag, if is a valid Object
     backspace           : true,           // false / true / "edit"
     skipInvalid         : false,          // If `true`, do not add invalid, temporary, tags before automatically removing them
     pasteAsTags         : true,           // automatically converts pasted text into tags. if "false", allows for further text editing
