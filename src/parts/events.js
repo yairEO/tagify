f102c7b26fb6fe668c97f11a9f45619ea3665501
Yair Even Or <vsync.design@gmail.com>
2025-01-10 18:00:12 +0100

    fix: remove unreachable return statement in events.js to streamline tag input handling

--- src/parts/events.js
+++ src/parts/events.js
@@ -814,7 +814,6 @@ export default {
 
                     if( result ){
                         this.injectAtCaret(result, window.getSelection().getRangeAt(0))
-                        return
 
                         if( this.settings.mode == 'mix' ){
                             this.events.callbacks.onMixTagsInput.call(this, e);
